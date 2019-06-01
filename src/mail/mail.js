bc3f4977cf7636585266fbe1fa39b4de22b1283f
Andrealoag <andrea.lopez.agurto@gmail.com>
2019-06-01 14:55:16 -0500

    envio de correo

--- src/mail/mail.js
+++ src/mail/mail.js
@@ -1,58 +1,58 @@
-export {} 
-var nodemailer = require('nodemailer');
+// export {funcion de datos} from ; 
+// var nodemailer = require('nodemailer');
 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'tucorreo@gmail.com',
-    pass: 'tucontraseña'
-  }
-});
+// var transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: 'tucorreo@gmail.com',
+//     pass: 'tucontraseña'
+//   }
+// });
 
-var mensaje = "Hola desde nodejs...";
+// var mensaje = "Hola desde nodejs...";
 
-var mailOptions = {
-  from: 'tucorreo@gmail.com',
-  to: 'mi-amigo@yahoo.com',
-  subject: 'Reporte semanal de casos de niños en la calle',
-  text: mensaje
-};
+// var mailOptions = {
+//   from: 'tucorreo@gmail.com',
+//   to: 'mi-amigo@yahoo.com',
+//   subject: 'Reporte semanal de casos de niños en la calle',
+//   text: mensaje
+// };
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email enviado: ' + info.response);
-  }
-});
-
-
-
-var nodemailer = require('nodemailer');
+// transporter.sendMail(mailOptions, function(error, info){
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('Email enviado: ' + info.response);
+//   }
+// });
 
 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'tucorreo@gmail.com',
-    pass: 'tucontraseña'
-  }
-});
 
-var mensaje =  `Hola desde mas calles,menos trabajo...`
-;
+// var nodemailer = require('nodemailer');
 
-var mailOptions = {
-  from: 'tucorreo@gmail.com',
-  to: 'mi-amigo@yahoo.com',
-  subject: 'Asunto Del Correo',
-  text: mensaje
-};
 
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email enviado: ' + info.response);
-  }
-});
+// var transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: 'tucorreo@gmail.com',
+//     pass: 'tucontraseña'
+//   }
+// });
+
+// var mensaje =  `Hola desde mas calles,menos trabajo...`
+// ;
+
+// var mailOptions = {
+//   from: 'tucorreo@gmail.com',
+//   to: 'mi-amigo@yahoo.com',
+//   subject: 'Asunto Del Correo',
+//   text: mensaje
+// };
+
+// transporter.sendMail(mailOptions, function(error, info){
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('Email enviado: ' + info.response);
+//   }
+// });
