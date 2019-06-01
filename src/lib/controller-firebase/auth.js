02bd50cb6b8e87638d1c9d01ac02ca8934c9dc6c
Stefany2210 <stefany.liseth@gmail.com>
2019-06-01 13:34:58 -0500

    agregando funcion google  de firebase

--- src/lib/controller-firebase/auth.js
+++ src/lib/controller-firebase/auth.js
@@ -1,3 +1,8 @@
 export const funcLogin = (emailLogIn, passwordLogIn) => firebase.auth().signInWithEmailAndPassword(emailLogIn, passwordLogIn);
 
 export const funcRegister = (emailSignIn, passwordSignIn) => firebase.auth().createUserWithEmailAndPassword(emailSignIn, passwordSignIn);
+
+export const funcGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    return firebase.auth().signInWithPopup(provider)
+  };
