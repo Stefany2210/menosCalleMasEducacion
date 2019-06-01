1efbc66a7210150929254fefdf13eed99530c3b0
Stefany2210 <stefany.liseth@gmail.com>
2019-06-01 14:27:15 -0500

    agregabdo rutas

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,19 @@
 
+const changeRoute = (hash) => {
+    console.log(hash);
+    const mainSection = document.getElementById('main');
+    mainSection.innerHTML = '';
+  
+    switch (hash) {
+      case '#':
+      case '':
+      case '#/': {
+        return mainSection.appendChild(components.login());
+      }
+    };
+
+
+
 export const initRoute = () => {
     changeRoute(window.location.hash);
     window.addEventListener('hashchange', () => changeRoute(window.location.hash))
