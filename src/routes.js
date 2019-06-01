8eb32c8d4f7a97e70453bf67c22f99dadac4d27e
Judith18 <judicriss18@gmail.com>
2019-06-01 15:16:50 -0500

    register

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,39 @@
 import { components } from "./ui/index.js";
+import { activeUser, currentUser } from "./lib/controller-firebase/auth.js"
+import { getRealTimePost } from './lib/controller-firebase/posts.js'
+
 const changeRoute = (hash) => {
-    console.log(hash);
-    const mainSection = document.getElementById('main');
-    mainSection.innerHTML = '';
-  
-    switch (hash) {
-      case '#':
-      case '':
-      case '#/': {
-        return mainSection.appendChild(components.login());
-      }
+  console.log(hash);
+  const mainSection = document.getElementById('main');
+  mainSection.innerHTML = '';
+
+  switch (hash) {
+    case '#':
+    case '':
+    case '#/': {
+      return mainSection.appendChild(components.login());
     };
+    case '#/register': {
+      return mainSection.appendChild(components.register());
+    };
+    case '#/content': {
+      activeUser(() => {
+        if (currentUser()) {
 
+          getRealTimePost(posts => {
+            mainSection.innerHTML = ''
+            return mainSection.appendChild(components.content(posts));
+          })
+        }
+      })
+    };
+    // default:
+    //   return mainSection.appendChild(components.different());
+  }
 
+};
 
 export const initRoute = () => {
-    changeRoute(window.location.hash);
-    window.addEventListener('hashchange', () => changeRoute(window.location.hash))
-  };
+  changeRoute(window.location.hash);
+  window.addEventListener('hashchange', () => changeRoute(window.location.hash))
+};
---
+++ src/ui/register.js
@@ -0,0 +1,32 @@
+import { register } from '../lib/view-controllers/auth.js'
+
+export const Register = () => {
+  const string =
+    `
+    <div class="container1 container-bg">
+      <img  class="someBg" src="assets/18984.jpg" alt="Some bg">
+    </div>
+
+    <div id="register" class="container1 px-5">
+      
+      <h4 class="px-4 subtitle-pass" >Registro de usuario </h4>
+      <div class="one-column">
+        <input id="name-signin" type="text" placeholder="Nombre y apellido" class="input-text" />
+        <input id="email-signin" type="email" placeholder="Email" class="input-text" />
+        <input id="password-signin" type="password" placeholder="Password" class="input-text" />
+        <p class="error-message"></p>
+        <button type="button" id="button-register" class="button-pass"> Registrarme </button>
+      </div>
+      <div class="fs-20 px-0">¿Ya estás registrado? <a href="#" id="back-login">Ingresa aquí</a> </div>
+    </div>
+  `;
+  const div = document.createElement('div')
+  div.className = 'fluid-flex';
+  div.innerHTML = string;
+
+  const nameSignin = div.querySelector('#name-signin');
+  const buttonRegisterEmail = div.querySelector('#button-register');
+  buttonRegisterEmail.addEventListener('click', () => register(nameSignin))
+
+  return div
+};
